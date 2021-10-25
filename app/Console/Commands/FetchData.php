d73deb733e1f8891bea274f393093ca47d4ccb61
Francesco Esposito <33671357+franjsco@users.noreply.github.com>
2021-10-26 00:09:55 +0200

    Add new artisan command

---
+++ app/Console/Commands/FetchData.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\FetchBitcoinData;
+use App\Jobs\FetchFeeData;
+use App\Jobs\FetchSentimentData;
+use Illuminate\Bus\Batch;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+
+class FetchData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fetch:all';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch (price, fee, sentiment) data';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $extractionId = Str::uuid();
+        
+        $batch = Bus::batch([
+            new FetchBitcoinData($extractionId),
+            new FetchSentimentData($extractionId),
+            new FetchFeeData($extractionId)
+        ])->then(function (Batch $batch) {
+            //Log::info('Ok');
+            // TODO: inserire tabella estrazioni
+        })->catch(function (Batch $batch) {
+            // TODO: gestire errori
+            //Log::error('Problemi');
+        })->dispatch(); 
+    }
+}
