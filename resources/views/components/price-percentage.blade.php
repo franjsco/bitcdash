66c9f2101606967bf3944732a35b30560e7d0296
Francesco Esposito <33671357+franjsco@users.noreply.github.com>
2021-11-08 22:40:00 +0100

    Remove background color from icon

--- resources/views/components/price-percentage.blade.php
+++ resources/views/components/price-percentage.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-white border-b-4 border-purple-600 rounded-xl shadow-lg p-8">
     <div class="flex flex-row items-center">
         <div class="flex-shrink pr-4">
-            <div class="rounded-full p-5 bg-yellow-600"><i class="fas fa-chart-line fa-2x fa-inverse"></i></div>
+            <div class="rounded-full p-5"><i class="fas fa-chart-line fa-2x"></i></div>
         </div>
         <div class="flex-1 text-right md:text-center">
             <h5 class="font-bold uppercase text-gray-600">24h</h5>
