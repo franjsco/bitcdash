d3c2700403578044bcd7e309a5354ea154a2bee7
Francesco Esposito <33671357+franjsco@users.noreply.github.com>
2021-11-10 01:30:04 +0100

    Implement color on sentiment bar

--- app/View/Components/Sentiment.php
+++ app/View/Components/Sentiment.php
@@ -20,6 +20,19 @@ class Sentiment extends Component
         $this->valueClassification = $valueClassification;
     }
 
+    public function getColorBasedOnSentiment()
+    {
+        $value = $this->value;
+
+        return match(true) {
+            $value <= 19 => 'bg-red-500',
+            $value >= 20 && $value <= 39 => 'bg-yellow-500',
+            $value >= 40 && $value <= 59 => 'bg-yellow-400',
+            $value >= 60 && $value <= 79  => 'bg-green-500',
+            $value >= 80 => 'bg-green-600'
+        };
+    }
+
     /**
      * Get the view / contents that represent the component.
      *
